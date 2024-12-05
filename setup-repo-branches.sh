f41d30d9252854ef1234ad360a20dd8093a34c4a
Zach Aller <zach_aller@intuit.com>
2024-12-05 12:16:09 -0600

    add setup script
    Signed-off-by: Zach Aller <zach_aller@intuit.com>

--- setup-repo-branches.sh
+++ setup-repo-branches.sh
@@ -1,19 +1,42 @@
 #!/bin/bash
 
-git checkout --orphan environment/development
 
+echo "Seting up environment/development"
+git checkout --orphan environment/development
 echo "Cleaning environment/development"
 git rm -rf .
-SHADEV=`git rev-parse main`
+SHA=`git rev-parse main`
 echo "Writing metadata file for development environment with dry sha: $SHADEV"
 cat <<- EOF > hydrator.metadata
-{"drySHA": "$SHADEV"}
+{"drySHA": "$SHA"}
 EOF
 git add hydrator.metadata
 git commit -m "inital environment setup"
 git push --force --set-upstream origin environment/development
 git checkout -b environment/development-next
 git push --force --set-upstream origin environment/development-next
+
+
+
+
+
+echo "Seting up environment/staging"
+git checkout --orphan environment/staging
+echo "Cleaning environment/staging"
+git rm -rf .
+SHA=`git rev-parse main`
+echo "Writing metadata file for development environment with dry sha: $SHADEV"
+cat <<- EOF > hydrator.metadata
+{"drySHA": "$SHA"}
+EOF
+git add hydrator.metadata
+git commit -m "inital environment setup"
+git push --force --set-upstream origin environment/staging
+git checkout -b environment/staging-next
+git push --force --set-upstream origin environment/staging-next
+
+
+
 
 #git checkout --orphan environment/staging
 #git checkout --orphan environment/production
