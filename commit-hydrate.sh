05f6f7d8ef66d4a0ecb8aafb28587b870a76bf83
Zach Aller <zach_aller@intuit.com>
2024-12-05 13:50:17 -0600

    setup script
    Signed-off-by: Zach Aller <zach_aller@intuit.com>

--- commit-hydrate.sh
+++ commit-hydrate.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 git commit -a -m "commiting dry changes"
+git push -u origin main
 
 kustomize build development/ > manifest.yaml-next
 git checkout environment/development-next
@@ -15,3 +16,24 @@ git add hydrator.metadata
 git add manifest.yaml
 git commit -a -m "hydration of dry commit $SHA"
 git push -u origin environment/development-next
+
+
+
+kustomize build staging/e2e > manifest.yaml-e2e-next
+kustomize build staging/integration > manifest.yaml-integration-next
+git checkout environment/development-next
+rm e2e/manifest.yaml
+mv manifest.yaml-e2e-next e2e/manifest.yaml
+mv manifest.yaml-inegration-next integration/manifest.yaml
+SHA=`git rev-parse main`
+echo "Writing metadata file for staging environment with dry sha: $SHA"
+cat <<- EOF > hydrator.metadata
+{"drySHA": "$SHA"}
+EOF
+git add hydrator.metadata
+git add e2e/
+git add integration/
+git commit -a -m "hydration of dry commit $SHA"
+git push -u origin environment/staging-next
+
+
